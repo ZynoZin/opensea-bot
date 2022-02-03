2e06989bfee2a4fddf7505658386c0424e9d7732
ZynoZin <zineddinelamziouik@gmail.com>
2022-02-03 01:51:49 +0100

    added traits

--- index.js
+++ index.js
@@ -49,25 +49,56 @@ mongoose.connect(config.dbURL, { useNewUrlParser: true, useUnifiedTopology: true
               .filter((event) => event.auction_type === 'dutch')
               .reverse();
             console.log(assetEvents.length + ' ' + collection.collectionSlug);
-
+          
             return Promise.all(
-              assetEvents.map((assetEvent) => {
-                let price = assetEvent.ending_price / Math.pow(10, 18);
-                price = price.toFixed(2);
-                //Create an embed for each assete that will be sent in Discord holding the link to the asset, the image and the price.
-                const assetName = assetEvent.asset.name || `${assetEvent.asset.collection.name} #${assetEvent.asset.token_id}`;
-                const assetEmbed = new MessageEmbed()
-                  .setColor('BLUE')
-                  .setTitle(
-                    `${assetName} is on sale for Ξ ${price}`
+              assetEvents.map(async (assetEvent) => {
+                // await sleep(1000)
+                let assetContractAdress = assetEvent.asset.asset_contract.address
+                let token = assetEvent.asset.token_id
+                let seller = assetEvent.seller.user ? assetEvent.seller.user.username : "Unnamed"
+                let sellerAddress = assetEvent.seller.address
+                let usdPrice = parseFloat(assetEvent.payment_token.usd_price) 
+                usdPrice = usdPrice.toFixed(2) 
+                await fetchAndRetryIfNecessary(
+                () => tokenBucket.acquireToken(() => api.get(
+                  `/asset/${assetContractAdress}/${token}`,
+                  { headers: { 'X-API-KEY': config.apiKey, Accept: 'application/json' } },
+                ))
+                ).then(async (response) => {
+                  let traits = response.data.traits
+                  // console.log(traits)
+                  let ethPrice = assetEvent.ending_price / Math.pow(10, 18);
+                  ethPrice = ethPrice.toFixed(2)
+                  usdPrice = (ethPrice * usdPrice).toFixed(2)
+                  //Create an embed for each assete that will be sent in Discord holding the link to the asset, the image and the price.
+                  const assetName = assetEvent.asset.name || `${assetEvent.asset.collection.name} #${assetEvent.asset.token_id}`;
+                  const assetEmbed = new MessageEmbed()
+                    .setColor('BLUE')
+                    .setTitle(
+                      `${assetName} is listed.`
                   )
-                  .setURL(`${assetEvent.asset.permalink}?ref=0x85E0aAfDb0D6516a007B3C16672B00731614d6AE`)
-                  .setImage(assetEvent.asset.image_url)
-                  .setTimestamp();
+                    .addFields(
+                      { name: `ETH price`, value: `${ethPrice}Ξ`, inline: true },
+                      { name: `USD price`, value: `$${usdPrice}`, inline: true },
+                      { name: `Seller`, value: `[${seller}](https://opensea.io/${sellerAddress})`, inline: true },
+                  )
+                    .addFields(
+                      
+                    )
+                    .setURL(`${assetEvent.asset.permalink}?ref=0x85E0aAfDb0D6516a007B3C16672B00731614d6AE`)
+                    .setImage(assetEvent.asset.image_url)
+                    .setTimestamp();
+                  if (traits.length != 0) {
+                    assetEmbed.addFields(traits.map((trait) => {
+                      return { name: `${trait.trait_type}`, value: `${trait.value}`, inline: true}
+                    }))
+                  }
+                  return client.channels.cache
+                    .get(collection.channelId)
+                    .send({ embeds: [assetEmbed] });
+                }).catch((error) => console.error(error))
                 
-                return client.channels.cache
-                  .get(collection.channelId)
-                  .send({ embeds: [assetEmbed] });
+                
               })
             );
           }).catch(console.error);
